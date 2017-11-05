ba3fb0988c21572675d26bd94f2a2725108cf4b1
Alexandre Masy <hello@alexandremasy.com>
2017-11-05 06:57:49 +0100

    Startup the converter logic

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./libs/markdown-to-pug');
+var blockquotes1 = '> one line';
+var blockquotes2 = '>> ...by using additional greater-than signs right next to each other...';
+var blockquotes3 = '> > > ...or with spaces between arrows.';
+
+md2pug = new (require('./lib/markdown-to-pug'))();
+md2pug.render(blockquotes3);
+
+module.exports = md2pug;
