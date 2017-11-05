52f491ca8929f09b97e416c1fd0368401551f033
Alexandre Masy <hello@alexandremasy.com>
2017-11-05 09:31:47 +0100

    Refactor the logic to remove duplicates

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var md = `**This is italic text**`;
+var md = `
+- item 1
+- item 2
+- item 3
+
+1. item 1
+2. item 2
+3. item 3`;
 
 md2pug = new (require('./lib/markdown-to-pug'))();
 let ret = md2pug.render(md);
