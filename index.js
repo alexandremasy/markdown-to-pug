36a713d0ee9c4f819515aada212236f6177c6c9b
Alexandre Masy <hello@alexandremasy.com>
2017-11-05 15:01:22 +0100

    Add an offset test and implementation

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var md = `
-- item 1
-- item 2
-- item 3
-
-1. item 1
+56. item 1
 2. item 2
 3. item 3`;
 
--- test/tests/lists.js
+++ test/tests/lists.js
@@ -6,16 +6,25 @@ describe('Lists', function()
 
   it('Ordered', function()
   {
-    let src = '- item 1\n- item 2\n- item 3';
-    let result = 'ul\n\tli\n\t\tp\n\t\t\titem 1\n\tli\n\t\tp\n\t\t\titem 2\n\tli\n\t\tp\n\t\t\titem 3';
+    let src = '1. item 1\n2. item 2\n3. item 3';
+    let result = 'ol\n\tli\n\t\tp\n\t\t\titem 1\n\tli\n\t\tp\n\t\t\titem 2\n\tli\n\t\tp\n\t\t\titem 3';
 
     assert.equal(md2pug.render(src), result);
   });
 
+  it('Ordered - Offset', function()
+  {
+    let src = '56. item 1\n2. item 2\n3. item 3';
+    let result = 'ol(start=56)\n\tli\n\t\tp\n\t\t\titem 1\n\tli\n\t\tp\n\t\t\titem 2\n\tli\n\t\tp\n\t\t\titem 3';
+
+    assert.equal(md2pug.render(src), result);
+  });
+
+
   it('Unordered', function()
   {
-    let src = '1. item 1\n2. item 2\n3. item 3';
-    let result = 'ol\n\tli\n\t\tp\n\t\t\titem 1\n\tli\n\t\tp\n\t\t\titem 2\n\tli\n\t\tp\n\t\t\titem 3';
+    let src = '- item 1\n- item 2\n- item 3';
+    let result = 'ul\n\tli\n\t\tp\n\t\t\titem 1\n\tli\n\t\tp\n\t\t\titem 2\n\tli\n\t\tp\n\t\t\titem 3';
 
     assert.equal(md2pug.render(src), result);
   });
