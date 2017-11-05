47e787a28477f550d29dd3586380357b743605d7
Alexandre Masy <hello@alexandremasy.com>
2017-11-05 17:57:31 +0100

    Multiline code

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-var md = 'Inline `coding`';
+var md = `Indented code
+
+    // Some comments
+    line 1 of code
+    line 2 of code
+    line 3 of code`;
+
+var md = `Lorem ipsum dolor sit amet, \nconsectetur adipiscing elit. Donec ut varius dui, ut placerat nisi. Aenean condimentum lorem massa, non molestie est porta cursus. Curabitur et mi vel turpis tincidunt tempor nec eu diam. Suspendisse egestas finibus augue et aliquet. Donec eget scelerisque tortor. Proin id mi varius, lacinia felis ac, porta mauris. Etiam sed nulla eget lectus ullamcorper eleifend. Curabitur ac iaculis augue, vitae facilisis urna. Aenean mollis consequat leo, in egestas ipsum dignissim at. Etiam ac magna et nulla posuere bibendum quis quis massa. Maecenas lacus magna, tempor at urna ac, finibus placerat nisi.`;
 
 md2pug = new (require('./lib/markdown-to-pug'))();
 let ret = md2pug.render(md);
-console.log(1, ret);
+console.log(ret);
 
 module.exports = md2pug;
