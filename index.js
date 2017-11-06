a0da3e27485f4ed7af367e7e9a08231555ba8366
Alexandre Masy <hello@alexandremasy.com>
2017-11-06 07:24:43 +0100

    Add preliminary support for table

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-var md = `\`\`\`
-Sample text here...
-\`\`\``;
+var md = `## Tables
+
+| Option | Description |
+| ------ | ----------- |
+| data   | path to data files to supply the data that will be passed into templates. |
+| engine | engine to be used for processing templates. Handlebars is the default. |
+| ext    | extension to be used for dest files. |
+`;
 
 
 md2pug = new (require('./lib/markdown-to-pug'))();
 let ret = md2pug.render(md);
-console.log(0, md);
-console.log(1, ret);
+console.log(md);
+console.log(ret);
 
 module.exports = md2pug;
