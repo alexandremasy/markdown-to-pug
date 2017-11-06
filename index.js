e2a7048f7bd9a2f9fbe131f7ec8be256a4893f59
Alexandre Masy <hello@alexandremasy.com>
2017-11-06 08:19:22 +0100

    Cleanup

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var md = `![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")`;
+// var md = `![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")`;
 
 md2pug = new (require('./lib/markdown-to-pug'))();
-let ret = md2pug.render(md);
-console.log(1, md);
-console.log(2, ret);
+// let ret = md2pug.render(md);
+// console.log(1, md);
+// console.log(2, ret);
 
 module.exports = md2pug;
