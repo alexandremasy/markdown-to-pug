1cad81ccb8cdf6ed87e4843625302f50cd8d61c4
Alexandre Masy <hello@alexandremasy.com>
2017-12-15 20:18:58 +0100

    Add the template support

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// var md = `<!-- +hello('arg1', 2, {'item': 'value'}) -->\n![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")`;
+var md = `<!-- extends /layout/base -->\n<!-- block abc -->\n\t![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")\n<!-- block def -->\n\t![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")`;
 
-// md2pug = new (require('./lib/markdown-to-pug'))();
-// let ret = md2pug.render(md);
-// console.log(md);
-// console.log();
-// console.log(ret);
+md2pug = new (require('./lib/markdown-to-pug'))();
+let ret = md2pug.render(md);
+console.log(md);
+console.log();
+console.log(ret);
 
 module.exports = require('./lib/markdown-to-pug');
