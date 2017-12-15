c86f4078fc4a99218551897c312f6a3d06fa721c
Alexandre Masy <hello@alexandremasy.com>
2017-12-15 19:31:11 +0100

    Fix import

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// var md = `<!-- +hello('arg1', 2, {'item': 'value'}) -->\n![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")\n<br/>asd`;
-// var md = `hello<br>hello`;
+// var md = `<!-- +hello('arg1', 2, {'item': 'value'}) -->\n![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")`;
 
-md2pug = new (require('./lib/markdown-to-pug'))();
+// md2pug = new (require('./lib/markdown-to-pug'))();
 // let ret = md2pug.render(md);
 // console.log(md);
 // console.log();
 // console.log(ret);
 
-module.exports = md2pug;
+module.exports = require('./lib/markdown-to-pug');
